77e9f73c7248eb782738904cedc3d7d773e1d91d
Edgar Lorente <edgar_lorentemartin@iescrlesvallbona.cat>
2024-04-03 16:55:13 +0200

    creacion de mas cosas

--- app/src/main/java/com/example/tarickalia/Register.kt
+++ app/src/main/java/com/example/tarickalia/Register.kt
@@ -2,17 +2,18 @@ package com.example.tarickalia
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.tarickalia.databinding.ActivityMainBinding
+import com.example.tarickalia.databinding.ActivityRegisterBinding
 
+class Register : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -21,7 +22,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.register.setOnClickListener() {
-            startActivity(Intent(this, Register::class.java))
+            val password = binding.password.text.toString()
+            val confirmPassword = binding.confirmarpassword.text.toString()
+
+            if (password == confirmPassword) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }

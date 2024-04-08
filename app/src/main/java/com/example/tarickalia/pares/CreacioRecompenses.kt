bd1065011733320136235fe428bd146543933849
Edgar Lorente <edgar_lorentemartin@iescrlesvallbona.cat>
2024-04-08 17:57:50 +0200

    avances

---
+++ app/src/main/java/com/example/tarickalia/pares/CreacioRecompenses.kt
@@ -0,0 +1,71 @@
+package com.example.tarickalia.pares
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.tarickalia.R
+import com.example.tarickalia.databinding.ActivityCreacioRecompensesBinding
+import com.google.android.material.navigation.NavigationView
+
+class CreacioRecompenses : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreacioRecompensesBinding
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreacioRecompensesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val usernamerebut = intent.getStringExtra("username")
+        binding.nompares.text = usernamerebut
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.GestioFills -> {
+                    val intent = Intent(this, GestioFamilia::class.java)
+                    intent.putExtra("username", binding.nompares.text.toString())
+                    startActivity(intent)
+                }
+                R.id.CreacioTasca -> {
+                    val intent = Intent(this, CreacioTasques::class.java)
+                    intent.putExtra("username", binding.nompares.text.toString())
+                    startActivity(intent)
+                }
+                R.id.CreacioRecompenses -> {
+                    val intent = Intent(this, CreacioRecompenses::class.java)
+                    intent.putExtra("username", binding.nompares.text.toString())
+                    startActivity(intent)
+                }
+                R.id.Puntuacions -> {
+                    val intent = Intent(this, PuntuacionsPares::class.java)
+                    intent.putExtra("username", binding.nompares.text.toString())
+                    startActivity(intent)
+                }
+                R.id.TasquesCompletes -> {
+                    val intent = Intent(this, TasquesCompletesPares::class.java)
+                    intent.putExtra("username", binding.nompares.text.toString())
+                    startActivity(intent)
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+        binding.gohome.setOnClickListener {
+            val intent = Intent(this, HomePares::class.java)
+            intent.putExtra("username", usernamerebut)
+            startActivity(intent)
+        }
+
+        binding.menuopen.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+    }
+}
